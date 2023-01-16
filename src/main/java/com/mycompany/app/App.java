24b27fb14f9819562f22e6d0f227db07c7fffaa2
Yong Wah <yongwahooi97@gmail.com>
2023-01-16 16:43:23 +0800

    My second change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("New changes");
+        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
